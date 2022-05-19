0e8713fc966d78b46fb35d3c5a38c4f2d48888f3
tsitsiflora <tsitsimunikwa97@gmail.com>
2022-05-19 16:34:42 +0200

    letsssssss go!

---
+++ PythonScripts/if_else.py
@@ -0,0 +1,10 @@
+#this script accepts an age and returns whether the person is allowed to drive or not
+min_driving_age = 18
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+
+'''Print {name} is allowed to drive or {name} is not allowed to drive
+    based to the check against the constant min_driving_age'''
+is_allowed = 'is allowed' if age >= min_driving_age else 'is not allowed'
+print(f"{name} {is_allowed} to drive")
+
