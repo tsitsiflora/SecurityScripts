0b38e298b518ff0aba428349750607ec31af543c
tsitsiflora <tsitsimunikwa97@gmail.com>
2022-05-20 11:42:36 +0200

    Split and join: tested and passed

--- PythonScripts/split_join.py
+++ PythonScripts/split_join.py
@@ -1,1 +1,15 @@
-#this script splits ang joins text together
+#this script splits and joins text together
+message = """Hello world!
+My name is Tsitsi.
+I am a Security Engineer.
+I like pentesting so much.
+And coding as well."""
+
+def split_and_join(message=message):
+    '''Split the message by newline, then join with | (pipe),
+        return the new output'''
+    lines = message.split('\n')
+    return '|'.join(lines)
+
+new_message = split_and_join(message)
+print(new_message)
