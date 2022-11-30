e02a23f320e11bb993f5b9094d6e5984843be6c0
tsitsiflora <tsitsimunikwa97@gmail.com>
2022-11-30 15:16:25 +0200

    50 Days of Python

---
+++ 50DaysOfPython/divide_or_square.py
@@ -0,0 +1,16 @@
+#Write a function called divide_or_square that takes one
+#argument (a number), and returns the square root of the number
+#if it is divisible by 5, returns its remainder if it is not divisible by
+#5.
+
+
+def divide_or_square(n):
+    res = 0
+    if n % 5 == 0:
+        square_root = n ** 0.5
+        return f'The square root is {square_root}'
+    else: 
+        remainder = n % 5
+        return f'The remainder is {remainder}'
+
+print(divide_or_square(14))
